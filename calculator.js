cfc05ee56bd2410fd965bf2f657557efe9b5e8e0
unknown <kevinpena421@gmail.com>
2022-10-06 16:39:18 -0700

    solved issue and completed MVP

--- calculator.js
+++ calculator.js
@@ -1,58 +1,42 @@
-
-  let FiveDecimal = 2;
-  let TenDecimal = 3;
-  let FifteenDecimal = 10;
-  let TwentyDecimal = 5;
-  let ThirtyDecimal = 6;
-  let FortyDecimal = 7; 
+  let FiveDecimal = 0.5;
+  let TenDecimal = 0.10;
+  let FifteenDecimal = 0.15;
+  let TwentyDecimal = 0.20;
+  let ThirtyDecimal = 0.30;
+  let FortyDecimal = 0.40; 
 
 let button = document.getElementById("button-submit")
 addEventListener("submit", function () {
     event.preventDefault();
     
     let MoneyInput = document.querySelector("input").value;
+    let Tip = document.getElementById("Tip-select")
     
-    // Multiplying FiveDecimal by Input value 
+    // Multiplying Decimals by Input value 
    
-    
-    
-    
-   /* let Tip = document.getElementById("Tip-select").value
-    let FiveTotal = MoneyInput * FiveDecimal;
-    let Output = document.querySelector('output').value = FiveTotal */
-   /* let Tip = document.getElementById("Tip-select").value
-    
-    if (TenDecimal === TenDecimal) {
-            let TenTotal = (MoneyInput * TenDecimal);
-            let Output = document.querySelector('output').value = TenTotal;
-        } else if (FifteenDecimal === FifteenDecimal) {
-            let FifteenTotal = (MoneyInput * FifteenDecimal);
-            let Output = document.querySelector('output').value = FifteenTotal;
-        }
-     */   
+    if (Tip.value === '5%') {
+        let FiveTotal = MoneyInput * FiveDecimal;
+        let Output = document.querySelector('output').value = FiveTotal
 
-});
-       addEventListener('submit',function() {
-        event.preventDefault();
-
-        MoneyInput = document.querySelector('input').value;
-
-        let TenTotal = (MoneyInput * TenDecimal);
-        let Output = document.querySelector('output').value = TenTotal;
-      
-           });
-    let Fifteen = document.getElementById("Tip-select").value
-      addEventListener('submit',function() {
-        let FifteenTotal = (MoneyInput * FifteenDecimal);
-        let Output = document.querySelector('output').value = FifteenTotal;
-      
-           });
-
-
-
-
-
-
-//let val = MoneyInput.valueOf();
-//    let Total = (MoneyInput * FiveDecimal);
-//    console.log(Total);
+    }
+    if (Tip.value === '10%') {
+        let TenTotal = MoneyInput * TenDecimal;
+        let Output = document.querySelector('output').value = TenTotal        
+    }
+    if (Tip.value === '15%') {
+        let FifteenTotal = MoneyInput * FifteenDecimal;
+        let Output = document.querySelector('output').value = FifteenTotal        
+    }
+    if (Tip.value === '20%') {
+        let TwentyTotal = MoneyInput * TwentyDecimal;
+        let Output = document.querySelector('output').value = TwentyTotal        
+    }
+    if (Tip.value === '30%') {
+        let ThirtyTotal = MoneyInput * ThirtyDecimal;
+        let Output = document.querySelector('output').value = ThirtyTotal        
+    }
+    if (Tip.value === '40%') {
+        let FortyTotal = MoneyInput * FortyDecimal;
+        let Output = document.querySelector('output').value = FortyTotal        
+    }
+});   
